20732a59228fb55eb3c27b8cb25e47f1d0f45e40
Terr <megakek@gmail.com>
2012-02-07 00:52:49 +0100

    Update tests with CaesarCracker

--- tests/caesar.py
+++ tests/caesar.py
@@ -21,13 +21,14 @@ def diff(a, b):
     # Only show differences
     filtered = list([x for x in result if x.startswith('+') or x.startswith('-') ])
 
-    output = 'Assertion failed. Differences:\n'
-    for i in xrange(0, len(filtered) - 1, 2):
+    num_diffs_shown = min(6, len(filtered) - 1)
+    output = 'Assertion failed. Differences (showing %d):\n' % (num_diffs_shown / 2)
+    for i in xrange(0, num_diffs_shown, 2):
         output += '%s\n%s\n\n' % (filtered[i], filtered[i+1])
     return output
 
 class CaesarCipherTest(TestCase):
-    def test_english(self):
+    def test_all_uppercase(self):
         plain = load_text('english_plain.txt')
         expected = load_text('english_encrypted.txt')
 
@@ -38,7 +39,7 @@ class CaesarCipherTest(TestCase):
         encrypted = cipher.encrypt(plain)
         self.assertEqual(encrypted, expected, diff(encrypted, expected))
 
-    def test_dutch(self):
+    def test_source_not_uppercase(self):
         plain = load_text('dutch_plain.txt')
         expected = load_text('dutch_encrypted.txt')
 
@@ -46,3 +47,44 @@ class CaesarCipherTest(TestCase):
         cipher = CaesarCipher(shift=3)
         encrypted = cipher.encrypt(plain)
         self.assertEqual(encrypted, expected, diff(encrypted, expected))
+
+    def test_special_characters(self):
+        plain = load_text('esperanto_plain.txt')
+        expected = load_text('esperanto_encrypted.txt')
+
+        # Test with a shift of 3
+        cipher = CaesarCipher(shift=3)
+        encrypted = cipher.encrypt(plain)
+        self.assertEqual(encrypted, expected, diff(encrypted, expected))
+
+
+class CaesarCrackerTest(TestCase):
+    """Test for Caesar cipher cracker."""
+
+    def test_english(self):
+        plain = load_text('english_plain.txt')
+        encrypted = load_text('english_encrypted.txt')
+
+        cracker = CaesarCracker(encrypted)
+        result = cracker.run()
+        self.assertEqual(result, plain, diff(result, plain))
+
+    def test_dutch(self):
+        plain = load_text('dutch_plain.txt').upper() # Source is in mixed case
+        encrypted = load_text('dutch_encrypted.txt')
+
+        cracker = CaesarCracker(encrypted)
+        result = cracker.run()
+        self.assertEqual(result, plain, diff(result, plain))
+
+    def test_esperanto(self):
+        """Esperanto has a higher 'A' frequency instead of 'E', like most
+        Western languages.
+        """
+        plain = load_text('esperanto_plain.txt').upper() # Source is in mixed case
+        encrypted = load_text('esperanto_encrypted.txt')
+
+        cracker = CaesarCracker(encrypted)
+        result = cracker.run()
+        self.assertEqual(result, plain, diff(result, plain))
+
