c9a0f5dc6b6bace2b3d83d4abe5606e6e3c470c3
Terr <megakek@gmail.com>
2012-02-15 00:55:44 +0100

    Loop through languages to determine best decryption result

--- crackers/caesar.py
+++ crackers/caesar.py
@@ -2,6 +2,8 @@ from base import SubstitutionCipherCracker
 
 from ciphers import CaesarCipher
 
+from crypto.exceptions import CrackerException
+
 
 class CaesarCracker(SubstitutionCipherCracker):
     """Provides functionality for cracking a CaesarCipher."""
@@ -17,13 +19,54 @@ class CaesarCracker(SubstitutionCipherCracker):
         the text is in.
         """
 
-        # Count letter frequency
-        frequencies = self.count_letter_frequency(self.text)
-        encrypted_common_letter = self.most_common_letter(frequencies)
+        best_language = None
+        best_result = ''
+        best_score = 0
 
-        # Calculate shift for what we assume is the 'E'
-        shift = self.get_letter_shift(encrypted_common_letter, 'E')
+        for language in self.languages:
+            # Count letter frequency
+            frequencies = self.count_letter_frequency(self.text)
+            encrypted_common_letter = self.most_common_letter(frequencies)
 
-        self.cipher.shift = shift
-        return self.cipher.decrypt(self.text)
+            # Calculate shift for the most common encrypted letter and letter in
+            # the current language
+            shift = self.get_letter_shift(encrypted_common_letter,
+                                          language.get_common_letters()[0].upper())
 
+            self.cipher.shift = shift
+            result = self.cipher.decrypt(self.text)
+
+            # See if words in the decrypted text match any of the most common
+            # words of the language and keep track of the score
+            common_words = [word.upper() for word in language.get_common_words()]
+            score = 0
+
+            for word in result.split():
+                if word in common_words:
+                    score += 1
+
+            if score > 0:
+                if score == best_score:
+                    raise CrackerException(
+                        'Cannot determine language as two languages got the same word ' \
+                        'score (%(score)d): %(lang1)s and %(lang2)s. Try adding more most common words ' \
+                        'to both languages' % {
+                            'score': score,
+                            'lang1': best_language,
+                            'lang2': language
+                    })
+                elif score > best_score:
+                    best_score = score
+                    best_language = language
+                    best_result = result
+
+        if best_score == 0:
+            raise CrackerException(
+                'Could not judge decryption results as none of ' \
+                'the registered languages returned a ' \
+                'word score. Try adding more languages or more most ' \
+                'common words.'
+            )
+
+        # Return our best guess as to the result
+        return best_result
